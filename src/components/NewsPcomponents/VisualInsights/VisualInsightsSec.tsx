4c2105fe1fa3098dbd274080b680ca1afdde09d6
Mitya <mitywaflow@gmail.com>
2026-02-07 21:12:04 +0100

    update

--- src/components/NewsPcomponents/VisualInsights/VisualInsightsSec.tsx
+++ src/components/NewsPcomponents/VisualInsights/VisualInsightsSec.tsx
@@ -2,7 +2,7 @@ import subImgFirst from "../../../assets/NewsPageImg/VisInsights/subContainer1.p
 import subImgSecond from "../../../assets/NewsPageImg/VisInsights/subContainer2.png";
 import subImgThird from "../../../assets/NewsPageImg/VisInsights/subContainer3.png";
 import subImgFourth from "../../../assets/NewsPageImg/VisInsights/subContainer4.png";
-import Pattern from "../../UI/Pattern/Pattern";
+
 export default function VisualInsightsSec() {
   const SubContainers = [
     {
@@ -28,32 +28,30 @@ export default function VisualInsightsSec() {
   ];
 
   return (
-    <Pattern>
-      <section className="max-w-7xl mx-auto visual-bg  text-white px-6 py-20">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 ">
-          {SubContainers.map((item, index) => (
-            <div
-              key={index}
-              className="rounded-xl overflow-hidden bg-[#141414] border border-[#2A2A2A] hover:border-[#3A3A3A] transition-all duration-300 hover:-translate-y-1 hover:shadow-xl"
-            >
-              <div className="relative w-full h-52 overflow-hidden">
-                <img
-                  src={item.img}
-                  alt={item.title}
-                  className="w-full h-full object-cover transition-transform duration-500 hover:scale-105"
-                />
-              </div>
-
-              <div className="p-5">
-                <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
-                <p className="text-sm text-gray-400 leading-relaxed">
-                  {item.text}
-                </p>
-              </div>
+    <section className="max-w-7xl mx-auto visual-bg  text-white px-6 py-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 ">
+        {SubContainers.map((item, index) => (
+          <div
+            key={index}
+            className="rounded-xl overflow-hidden bg-[#141414] border border-[#2A2A2A] hover:border-[#3A3A3A] transition-all duration-300 hover:-translate-y-1 hover:shadow-xl"
+          >
+            <div className="relative w-full h-52 overflow-hidden">
+              <img
+                src={item.img}
+                alt={item.title}
+                className="w-full h-full object-cover transition-transform duration-500 hover:scale-105"
+              />
             </div>
-          ))}
-        </div>
-      </section>
-    </Pattern>
+
+            <div className="p-5">
+              <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
+              <p className="text-sm text-gray-400 leading-relaxed">
+                {item.text}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
